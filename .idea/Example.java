a353bd77b5e15c3c7ff0e0f0d464e3c12cd57895
hef1de <byehlip@gmail.com>
2026-01-30 23:10:33 +0400

    add new class

--- .idea/Example.java
+++ .idea/Example.java
@@ -1,5 +1,12 @@
 public class Example{
     public static void main(String[] args){
-        System.out.println("Test");
+        System.out.println("Test is begining");
+    }
+
+}
+
+class Calculete{
+    public int pluss(int firstNum, int secNum){
+        return firstNum + secNum;
     }
 }
